e940de8b93350f81e547303e0640f5636fbe093f
Jan Machacek <jan.machacek@gmail.com>
2014-04-16 15:07:54 +0100

    Hello, world service and app

--- src/main/scala/org/eigengo/phillyete/helloworld.scala
+++ src/main/scala/org/eigengo/phillyete/helloworld.scala
@@ -6,5 +6,21 @@ import spray.can.Http
 import akka.io.IO
 import akka.actor.Actor.Receive
 
+class HelloWorldService extends Actor {
+  def receive: Receive = {
+    case r: HttpRequest =>
+      sender ! HttpResponse(entity = HttpEntity("Hello, world"))
+    case _: Http.Connected =>
+      sender ! Http.Register(self)
+  }
+}
+
 object HelloWorld extends App {
+  val system = ActorSystem()
+  val service = system.actorOf(Props[HelloWorldService])
+
+  IO(Http)(system) ! Http.Bind(service, "0.0.0.0", port = 8080)
+
+  Console.readLine()
+  system.shutdown()
 }
