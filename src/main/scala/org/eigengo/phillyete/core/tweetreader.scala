99f11c49908908122f70864433b01aa4bd234627
Jan Machacek <jan.machacek@gmail.com>
2014-04-16 16:22:28 +0100

    Tweet reader

--- src/main/scala/org/eigengo/phillyete/core/tweetreader.scala
+++ src/main/scala/org/eigengo/phillyete/core/tweetreader.scala
@@ -12,6 +12,44 @@ import scala.util.Try
 import spray.can.Http
 import akka.io.IO
 
+trait TwitterAuthorization {
+  def authorize: HttpRequest => HttpRequest
+}
+
+trait OAuthTwitterAuthorization extends TwitterAuthorization {
+  import OAuth._
+  val home = System.getProperty("user.home")
+  val lines = Source.fromFile(s"$home/.twitter/phillyete2014").getLines().toList
+
+  val consumer = Consumer(lines(0), lines(1))
+  val token = Token(lines(2), lines(3))
+
+  val authorize: HttpRequest => HttpRequest = oAuthAuthorizer(consumer, token)
+}
+
+object TweetReaderActor {
+  val twitterUri = Uri("https://stream.twitter.com/1.1/statuses/filter.json")
+}
+
+class TweetReaderActor(uri: Uri, receiver: ActorRef) extends Actor with TweetMarshaller {
+  this: TwitterAuthorization =>
+  val io = IO(Http)(context.system)
+  val sentimentAnalysis = new SentimentAnalysis with CSVLoadedSentimentSets
+
+  def receive: Receive = {
+    case query: String =>
+      val post = HttpEntity(ContentType(MediaTypes.`application/x-www-form-urlencoded`), s"track=$query")
+      val rq = HttpRequest(HttpMethods.POST, uri = uri, entity = post) ~> authorize
+      sendTo(io).withResponsesReceivedBy(self)(rq)
+    case ChunkedResponseStart(_) =>
+    case MessageChunk(entity, _) =>
+      TweetUnmarshaller(entity) match {
+        case Right(tweet) => receiver ! sentimentAnalysis.onTweet(tweet)
+        case _            =>
+      }
+    case _ =>
+  }
+}
 
 
 
