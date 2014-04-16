6c0ccc209358920389b8d52ae4b063deeb61f7d7
Jan Machacek <jan.machacek@gmail.com>
2014-04-16 15:08:26 +0100

    Hello, world service test

--- src/test/scala/org/eigengo/phillyete/HelloWorldServiceSpec.scala
+++ src/test/scala/org/eigengo/phillyete/HelloWorldServiceSpec.scala
@@ -5,6 +5,16 @@ import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import spray.http.{HttpEntity, HttpResponse, HttpRequest}
 
-class HelloWorldServiceSpec {
+class HelloWorldServiceSpec extends TestKit(ActorSystem())
+  with SpecificationLike with ImplicitSender {
+
+  val service = TestActorRef[HelloWorldService]
+
+  "Any request" should {
+    "Reply with Hello, world" in {
+      service ! HttpRequest()
+      expectMsgType[HttpResponse].entity mustEqual HttpEntity("Hello, world")
+    }
+  }
 
 }
